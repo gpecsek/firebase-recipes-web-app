ccb3488ec12a1661e8308239b6639ab4af9545fd
Gabor Pécsek <gabor.pecsek@gmail.com>
2023-05-19 17:59:53 +0200

    started to add the AddEditRecipeForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { useState } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
 import LoginForm from "./components/LoginForm";
+import AddEditRecipeForm from "./components/AddEditRecipeForm";
 
 import "./App.css";
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 function App() {
   const [user, setUser] = useState(null);
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
 
+  async function handleAddRecipe(newRecipe) {
+    try {
+      const response = await FirebaseFirestoreService.createDocument(
+        "recipes",
+        newRecipe
+      );
+
+      // TODO: fetch new recipes frm firestore
+
+      alert(`Succesfully created a recipe with and ID = ${response.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
         <LoginForm existingUser={user} />
+      </div>
+      <div className="main">
+        <AddEditRecipeForm handleAddRecipe={handleAddRecipe} />
       </div>
     </div>
   );
